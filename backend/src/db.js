7fe0fd083434b0073d2246a1b3fbbedf63406f51
Ananda Rizki <anandarizn@gmail.com>
2026-02-03 00:35:17 +0700

    Enhance db singleton

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,5 +1,13 @@
 const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient();
+const globalForPrisma = global;
+
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["error", "warn"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 module.exports = prisma;
